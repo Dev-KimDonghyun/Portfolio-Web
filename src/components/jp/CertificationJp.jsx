ed3537ca4dc5bfb908dad0199deb64a55c60eb1f
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-30 12:48:03 +0900

    feat: Add CertificationJp Component Structure

---
+++ src/components/jp/CertificationJp.jsx
@@ -0,0 +1,11 @@
+const CertificationJp = () => {
+  return (
+    <div>
+      <h3>Certification</h3>
+      <p>2025.08.</p>
+      <p>JLPT 日本語能力試験 - N2</p>
+    </div>
+  );
+};
+
+export default CertificationJp;
