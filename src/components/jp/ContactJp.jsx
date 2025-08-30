ff9a8eb252f2bb7b40ea6cdc33fb23111ce72d68
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-30 11:59:52 +0900

    feat: Add ContactJp Component Structure

---
+++ src/components/jp/ContactJp.jsx
@@ -0,0 +1,16 @@
+const ContactJp = () => {
+  return (
+    <div>
+      <div>
+        <h3>GitHub</h3>
+        <p>https://github.com/Dev-KimDonghyun</p>
+      </div>
+      <div>
+        <h3>Email</h3>
+        <p>hyungus723@gmail.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default ContactJp;
