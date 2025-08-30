e8ec4892173afb94c459de57e93231f0dfc3dc78
Dev-KimDonghyun <hyungus723@gmail.com>
2025-08-30 12:04:24 +0900

    feat: Add EducationJp Component Structure

---
+++ src/components/jp/EducationJp.jsx
@@ -0,0 +1,11 @@
+const EducationJp = () => {
+  return (
+    <div>
+      <h3>Education</h3>
+      <p>2025.03. ~ 在学中</p>
+      <p>Yeungnam University College ソフトウェア融合科</p>
+    </div>
+  );
+};
+
+export default EducationJp;
