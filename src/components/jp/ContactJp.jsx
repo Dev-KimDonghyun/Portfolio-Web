b7e5ceb16c9a5bef1335a50fbb975e7ba0455585
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 16:23:16 +0900

    feat: Add GitHub and Email Hyperlinks in ContactJp.jsx

--- src/components/jp/ContactJp.jsx
+++ src/components/jp/ContactJp.jsx
@@ -1,16 +1,34 @@
 const ContactJp = () => {
   const contacts = [
-    { label: "GitHub", value: "github.com/Dev-KimDonghyun" },
-    { label: "Email", value: "hyungus723@gmail.com" },
+    {
+      label: "GitHub",
+      value: "github.com/Dev-KimDonghyun",
+      href: "https://github.com/Dev-KimDonghyun",
+    },
+    {
+      label: "Email",
+      value: "hyungus723@gmail.com",
+      href: "mailto:hyungus723@gmail.com",
+    },
   ];
 
   return (
     <div>
-      {contacts.map(({ label, value }) => (
+      {contacts.map(({ label, value, href }) => (
         <div key={label} className="my-2 md:my-0">
           <p className="text-md md:text-lg">
-            <span className="text-blue-700 text-lg md:text-xl mr-2">{label}</span><br className="block md:hidden"/>
-            {value}
+            <span className="text-blue-700 text-lg md:text-xl mr-2">
+              {label}
+            </span>
+            <br className="block md:hidden" />
+            <a
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline cursor-pointer"
+            >
+              {value}
+            </a>
           </p>
         </div>
       ))}
