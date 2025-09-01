2d8b8cc01de3bac2a479cb8988e8704ab88b8a87
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 16:15:02 +0900

    feat: Add Responsive Design in ContactJp.jsx

--- src/components/jp/ContactJp.jsx
+++ src/components/jp/ContactJp.jsx
@@ -7,9 +7,9 @@ const ContactJp = () => {
   return (
     <div>
       {contacts.map(({ label, value }) => (
-        <div key={label}>
-          <p className="text-lg">
-            <span className="text-blue-700 text-xl mr-2">{label}</span>
+        <div key={label} className="my-2 md:my-0">
+          <p className="text-md md:text-lg">
+            <span className="text-blue-700 text-lg md:text-xl mr-2">{label}</span><br className="block md:hidden"/>
             {value}
           </p>
         </div>
