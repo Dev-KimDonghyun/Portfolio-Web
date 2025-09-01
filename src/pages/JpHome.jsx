6b14d8664cb094ac64d7f99eebc9316402f51a2d
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-01 10:34:14 +0900

    feat: Refine Layout and Styling in JpHome.jsx

--- src/pages/JpHome.jsx
+++ src/pages/JpHome.jsx
@@ -7,15 +7,15 @@ import TechJp from "../components/jp/TechJp";
 
 const JpHome = () => {
   return (
-    <div className="font-jp bg-amber-50">
-      <TitleJp />
-      <div className="grid grid-cols-2">
-        <AboutJp />
-        <ContactJp />
-        <EducationJp />
-        <CertificationJp />
+    <div className="min-h-screen bg-amber-50 font-jp flex items-center">
+      <div className="mt-16 px-82 w-full flex flex-col gap-12">
+        <TitleJp />
+          <AboutJp />
+          <ContactJp />
+          <EducationJp />
+          <CertificationJp />
+        <TechJp />
       </div>
-      <TechJp />
     </div>
   );
 };
