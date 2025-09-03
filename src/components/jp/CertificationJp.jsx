d36222f7d605d598bb540e9683fe5800ec5ca7ed
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-03 16:55:47 +0900

    chore: Apply Base Styling to CertificationJp.jsx and EducationJp.jsx

--- src/components/jp/CertificationJp.jsx
+++ src/components/jp/CertificationJp.jsx
@@ -1,9 +1,9 @@
 const CertificationJp = () => {
   return (
     <div>
-      <h3>Certification</h3>
-      <p>2025.08.</p>
-      <p>JLPT 日本語能力試験 - N2</p>
+      <h3 className="text-2xl md:text-3xl text-emerald-800 font-semibold mb-2">Certification</h3>
+      <p className="text-sm sm:text-lg">2025.08.</p>
+      <p className="text-sm sm:text-lg">JLPT 日本語能力試験 - N2</p>
     </div>
   );
 };
--- src/components/jp/EducationJp.jsx
+++ src/components/jp/EducationJp.jsx
@@ -1,9 +1,9 @@
 const EducationJp = () => {
   return (
     <div>
-      <h3>Education</h3>
-      <p>2025.03. ~ 在学中</p>
-      <p>Yeungnam University College ソフトウェア融合科</p>
+      <h3 className="text-2xl md:text-3xl text-emerald-800 font-semibold mb-2">Education</h3>
+      <p className="text-sm sm:text-lg">2025.03. ~ 在学中</p>
+      <p className="text-sm sm:text-lg">Yeungnam University College <br className="inline md:hidden"/>ソフトウェア融合科</p>
     </div>
   );
 };
