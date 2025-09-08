a53c83c932604fc5a915cd76c29b3ff35eda8b89
Dev-KimDonghyun <hyungus723@gmail.com>
2025-09-08 20:11:32 +0900

    chore: Restore Backend Engineer Text on Page

--- src/components/TitleJp.jsx
+++ src/components/TitleJp.jsx
@@ -1,7 +1,13 @@
 const TitleJp = () => {
   return (
     <div className="text-lg sm:text-2xl md:text-3xl font-semibold">
-      <h1>こんにちは<br/>開発者志望の<span className="text-emerald-800">KimDonghyun</span>です</h1>
+      <h1>
+        こんにちは
+        <br />
+        バックエンドエンジニア志望の
+        <br />
+        <span className="text-emerald-800">KimDonghyun</span>です
+      </h1>
     </div>
   );
 };
